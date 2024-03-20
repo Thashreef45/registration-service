ed62ab9149245828f2da0dbe3524c27050d4f2bd
Alan <alan.varghese@giggr.app>
2024-03-20 20:11:52 +0530

    feat: mongodb init

---
+++ src/infrastructure/database/setup.ts
@@ -0,0 +1,17 @@
+'use strict';
+
+import env from '../config/environment.js';
+import mongoose from 'mongoose';
+
+const connectDB = async () => {
+    try {
+        const conn = await mongoose.connect(env.DATABASE_URL);
+        console.log(`🍃 [database] Established connection with MongoDB @ ${conn.connection.host}`);
+    } catch (error: any) {
+        console.error(`🍃 [database]  ${error.message}`);
+        console.log("Are you sure MongoDB is running?");
+        process.exit(1);
+    }
+};
+
+export default connectDB;
