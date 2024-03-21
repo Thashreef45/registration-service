ea14b59c45242427a0b416cfb490eeb93f11c7c8
Alan <alan.varghese@giggr.app>
2024-03-21 17:33:10 +0530

    feat: use case dependents

---
+++ src/use-cases/shared/AppError.ts
@@ -0,0 +1,16 @@
+import { StatusCode } from "./StatusCodes.js";
+
+export class AppError extends Error {
+    public readonly status: number;
+    public readonly isCatastrophic: boolean;
+
+    constructor(description: string, status: StatusCode, isCatastrophic: boolean = false) {
+        super(description);
+        Object.setPrototypeOf(this, new.target.prototype); // restore prototype chain
+
+        this.status = status;
+        this.isCatastrophic = isCatastrophic;
+
+        Error.captureStackTrace(this);
+    }
+}
