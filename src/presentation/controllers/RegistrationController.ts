4524d0bc8ad80af312919aa60aed411d6815f1f1
Alan <alan.varghese@giggr.app>
2024-03-26 14:09:33 +0530

    refactor: conventions, quality

--- src/presentation/controllers/RegistrationController.ts
+++ src/presentation/controllers/RegistrationController.ts
@@ -2,30 +2,26 @@ import { Request, Response } from "express";
 import asyncHandler from "express-async-handler";
 
 //* Interactors
-import CreateAccount from "../../use-cases/interactors/createAccount.js";
+import CreateAccount from "../../use-cases/interactors/CreateAccount.js";
 
 //* Dependencies
 import { UserRepositoryMongoDB } from "../../infrastructure/repositories/UserRepositoryMongoDB.js";
+import { GiggrSnowflake } from "../../infrastructure/services/GiggrSnowflakeAccountIdGenerator.js";
 
 const userRepository = new UserRepositoryMongoDB();
+const accountIdGenerator = new GiggrSnowflake();
 
-export const RegisterController = asyncHandler(_RegisterUser);
-
-//Creating a new account
+export const RegisterUser = asyncHandler(_RegisterUser);
 async function _RegisterUser(req: Request, res: Response): Promise<void> {
-  const interactor = new CreateAccount({ userRepository });
+  const interactor = new CreateAccount({ userRepository, accountIdGenerator });
 
   const data = {
     name: req.body.name,
     email: req.body.email,
     phone: req.body.phone,
-    dob: req.body.dateOfBirth,
+    dateOfBirth: req.body.dateOfBirth,
   };
 
   const output = await interactor.execute(data);
   res.json(output);
 }
-
-// export default Register
-
-// export default RegisterController
--- src/presentation/routers/registerRouter.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { RegisterController } from "../controllers/RegisterController.js";
-
-const registerRouter = Router();
-
-registerRouter.post("/", RegisterController);
-
-export default registerRouter;
