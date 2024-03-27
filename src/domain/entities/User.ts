d0d59ad28c541338a07d857ec15a40646c7fe211
sebastianthomas94 <53168297+sebastianthomas94@users.noreply.github.com>
2024-03-27 12:36:10 +0530

    update: changed email to optional
    during the otp verification the users data and otp must be saved to the users db. so had to change the email?.

--- src/domain/entities/User.ts
+++ src/domain/entities/User.ts
@@ -1,30 +1,33 @@
-
 interface UserParams {
-    accountId?: string;
-    name: string;
-    email: string;
-    phone: string;
-    dateOfBirth: Date;
+  accountId?: string;
+  name: string;
+  email?: string;
+  phone: string;
+  dateOfBirth: Date;
+  OTP?: string;
 }
 
 /** Definition for an user entity of the platform. */
 export class User {
-    /** A 16-digit numeric identifier assigned to a user of the platform. */
-    accountId?: string;
-    /** The full legal name of the user. */
-    name: string;
-    /** The email address (personal, work, or academic) of the user. */
-    email: string;
-    /** The phone number of the user. */
-    phone: string;
-    /** The date of birth of the user. */
-    dateOfBirth: Date;
+  /** A 16-digit numeric identifier assigned to a user of the platform. */
+  accountId?: string;
+  /** The full legal name of the user. */
+  name: string;
+  /** The email address (personal, work, or academic) of the user. */
+  email?: string;
+  /** The phone number of the user. */
+  phone: string;
+  /** The date of birth of the user. */
+  dateOfBirth: Date;
+  /** The date of birth of the user. */
+  OTP?: string;
 
-    constructor({ accountId, name, email, phone, dateOfBirth }: UserParams) {
-        this.accountId = accountId;
-        this.name = name;
-        this.email = email;
-        this.phone = phone;
-        this.dateOfBirth = dateOfBirth;
-    }
+  constructor({ accountId, name, email, phone, dateOfBirth, OTP }: UserParams) {
+    this.accountId = accountId;
+    this.name = name;
+    this.email = email;
+    this.phone = phone;
+    this.dateOfBirth = dateOfBirth;
+    this.OTP = OTP;
+  }
 }
