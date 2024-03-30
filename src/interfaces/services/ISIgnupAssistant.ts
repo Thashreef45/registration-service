335787eb2bfbfed927f58b2804f16d3d29fb8df3
Alan <alan.varghese@giggr.app>
2024-03-30 19:46:38 +0530

    feat: signup assistant interface #7

---
+++ src/interfaces/services/ISIgnupAssistant.ts
@@ -0,0 +1,24 @@
+import { Chat } from "../../domain/entities/Chat.js";
+
+export type ResponseType = {
+    message: string;
+    field: string | null;
+    fields: {
+        name: string | null;
+        email: string | null;
+        phone: string | null;
+    };
+}
+
+export interface ISignupAssistant {
+    /** Set a Chat entity as context. */
+    recall(context: Chat): boolean;
+    /** Retrieve the current context as a Chat entity. */
+    getContext(): Chat;
+    /** Compose a new message, but not send it. */
+    compose(message: string): boolean;
+    /** Send the composed message and get a response. */
+    getResponse(): Promise<ResponseType>;
+    /** Undo last two messages from context. */
+    rollback(): boolean;
+}
