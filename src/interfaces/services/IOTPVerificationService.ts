0bd956ea7675a495ef20d8e20c4fd2f8a008f80f
Alan <alan.varghese@giggr.app>
2024-03-31 00:35:24 +0530

    fix: combine otp services

--- src/interfaces/services/IOTPVerificationService.ts
+++
@@ -1,6 +0,0 @@
-import StatusCode from "../../use-cases/shared/StatusCodes.js";
-
-export interface IOTPVerificationService {
-  generateAndSend(phone: string): Promise<StatusCode>;
-  verify(phone: string, OTP: string): Promise<StatusCode>;
-}
--- src/interfaces/services/IOtpGenerator.ts
+++
@@ -1,3 +0,0 @@
-export interface IOTPGenerator {
-  generate(length?: number): string;
-}
