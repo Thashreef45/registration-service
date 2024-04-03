4ad9358a329f186523d93a436b931fe3ce279243
Alan <alan.varghese@giggr.app>
2024-04-03 20:01:01 +0530

    feat: schemas for all controllers

--- src/presentation/middlewares/validateSchema.ts
+++ src/presentation/middlewares/validateSchema.ts
@@ -1,6 +1,37 @@
 import { Request, Response, NextFunction } from 'express';
 import { z } from 'zod';
 
+interface Schema {
+    bodySchema?: z.ZodObject<any, any>;
+    paramsSchema?: z.ZodObject<any, any>;
+    querySchema?: z.ZodObject<any, any>;
+}
+
+function validateSchema(schema: Schema) {
+    return async (req: Request, res: Response, next: NextFunction) => {
+        console.log(req.params);
+        try {
+            if (schema.bodySchema) {
+                req.body = await schema.bodySchema.parseAsync(req.body);
+            }
+            if (schema.querySchema) {
+                req.query = await schema.querySchema.parseAsync(req.query);
+            }
+            if (schema.paramsSchema) {
+                req.params = await schema.paramsSchema.parseAsync(req.params);
+            }
+            next();
+        } catch (error) {
+            console.error(error);
+            if (error instanceof z.ZodError) {
+                const firstErrorMessage = error.errors[0].message;
+                return res.status(400).send({ error: firstErrorMessage });
+            }
+            next(error);
+        }
+    };
+}
+
 /** Validates `req.body` against given Zod Schema. */
 function validateBody(schema: z.ZodObject<any, any>) {
     return (req: Request, res: Response, next: NextFunction) => {
@@ -52,4 +83,5 @@ function validateQuery(schema: z.ZodObject<any, any>) {
     };
 }
 
+export default validateSchema;
 export { validateBody, validateParams, validateQuery };
