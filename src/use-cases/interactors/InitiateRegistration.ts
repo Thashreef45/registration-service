0461f0502035f069a3c92e5280fe192d943d53ce
Alan <alan.varghese@giggr.app>
2024-04-04 12:32:28 +0530

    feat: business logic --temp

--- src/use-cases/interactors/InitiateRegistration.ts
+++ src/use-cases/interactors/InitiateRegistration.ts
@@ -4,42 +4,65 @@ import { AppError } from "../shared/AppError.js";
 import IRegistrationRepository from "../../interfaces/repositories/IRegistrationRepository.js";
 import { Registration } from "../../domain/entities/Registration.js";
 import { IUUIDGenerator } from "../../interfaces/services/IUUIDGenerator.js";
+import IOrganizationRepository from "../../interfaces/repositories/IOrganizationRepository.js";
+import { Organization } from "../../domain/entities/Organization.js";
 
 export default class InitiateRegistration implements IUseCase<Input, Output> {
   private readonly registrationRepository: IRegistrationRepository;
+  // private readonly organizationRepository: IOrganizationRepository;
   private readonly uuidGenerator: IUUIDGenerator;
 
   constructor({ registrationRepository, uuidGenerator }: Dependencies) {
     this.registrationRepository = registrationRepository;
+    // this.organizationRepository = organizationRepository;
     this.uuidGenerator = uuidGenerator;
   }
 
   async execute({ entity, deviceId, locationId, networkId }: Input): Promise<Output> {
 
-    const uuid = this.uuidGenerator.generate();
-
+    const role = entity === "individual" ? "administrator" : "user";
+    
     // todo: consider checking the database for the same unique fingerprint
+    const userUUID = this.uuidGenerator.generate();
     const registration = new Registration({
-      uuid,
+      uuid: userUUID,
       entity,
+      role,
       deviceId,
       locationId,
       networkId
     });
 
-    const didPersist = await this.registrationRepository.persist(registration);
-    if (didPersist != StatusCode.CREATED) {
+    const didUserPersist = await this.registrationRepository.persist(registration);
+    if (didUserPersist != StatusCode.CREATED) {
       throw new AppError("Could not persist registration.", StatusCode.INTERNAL_ERROR);
     }
+    
+    // Industries & Institutes
+    // if (entity !== "individual") {
+    //   const orgUUID = this.uuidGenerator.generate();
+    //   const organization = new Organization({
+    //     uuid: orgUUID,
+    //     adminUUID: userUUID,
+    //     entity,
+    //     documents: "awaiting"
+    //   });
+
+    //   const didOrgPersist = await this.organizationRepository.persist(organization);
+    //   if (didOrgPersist != StatusCode.CREATED) {
+    //     throw new AppError("Could not persist organization.", StatusCode.INTERNAL_ERROR);
+    //   }
+    // }
 
     return {
-      signupId: uuid
+      signupId: userUUID
     }
   }
 }
 
 interface Dependencies {
   registrationRepository: IRegistrationRepository;
+  // organizationRepository: IOrganizationRepository;
   uuidGenerator: IUUIDGenerator;
 }
 
