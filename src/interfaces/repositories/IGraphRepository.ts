105cc3d1b534fa3f242167ffd0fe45ce2a8ef594
Alan <alan.varghese@giggr.app>
2024-04-06 07:43:13 +0530

    fix: graph repo types

--- src/interfaces/repositories/IGraphRepository.ts
+++ src/interfaces/repositories/IGraphRepository.ts
@@ -1,11 +1,11 @@
 import { BaseProfile } from "../../domain/entities/BaseProfile.js";
-import { Organization } from "../../domain/entities/Organization.js";
+import { OrganizationProfile } from "../../domain/entities/OrganizationProfile.js";
 import StatusCode from "../../use-cases/shared/StatusCodes.js";
 
 export default interface IGraphRepository {
-  findByGiggrId(giggrId: string): Promise<BaseProfile | Organization | null>;
+  findByGiggrId(giggrId: string): Promise<BaseProfile | OrganizationProfile | null>;
   findByPhone(phone: string): Promise<BaseProfile | null>;
   findByEmail(email: string): Promise<BaseProfile | null>;
   createBaseProfile(profile: BaseProfile): Promise<StatusCode>;
-  createOrganization(organization: Organization): Promise<StatusCode>;
+  createOrganization(organization: OrganizationProfile): Promise<StatusCode>;
 }
